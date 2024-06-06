947a8891e35cac9ed5a1ebacd7260d5f02f430f2
MegaChie <workspace.mega@gmail.com>
2024-06-06 13:14:38 -0700

    Adds doc string to user defined function\

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,6 +6,7 @@ T = TypeVar("T")
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
+    """Function defined in the project's last mandatory task"""
     if key in dct:
         return dct[key]
     else:
