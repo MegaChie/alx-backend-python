d158a8d5d508b9c42f9ff13b85faf28bac51b520
MegaChie <workspace.mega@gmail.com>
2024-06-06 09:09:29 -0700

    Returns the number as a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 3"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    type-annotated function that returns a string representation of a float
+    """
+    return str(n)
