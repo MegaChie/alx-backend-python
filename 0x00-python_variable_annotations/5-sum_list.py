47a1390a27876a3a40e2cd4d6249e10a9067089a
MegaChie <workspace.mega@gmail.com>
2024-06-06 09:32:34 -0700

    Returns the sum of list elements

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Type-annotated function that sums elements of the list"""
+    return sum(input_list)
