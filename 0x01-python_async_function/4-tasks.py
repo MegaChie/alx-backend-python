5093cc4802c60db9587a90c818c3a291dd40c07e
MegaChie <workspace.mega@gmail.com>
2024-06-10 12:32:40 -0700

    Copies and rename

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 1"""
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of all delays in an ascending order"""
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    tasks = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    res = []
+    for task in asyncio.as_completed(tasks):
+        res.append(await task)
+    return res
