9b38d6cb867c4b735243c589c6a8e6bd9df9f61c
MegaChie <workspace.mega@gmail.com>
2024-06-10 12:34:57 -0700

    Fixs typo with function name

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ import asyncio
 from typing import List
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of all delays in an ascending order"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     tasks = []
